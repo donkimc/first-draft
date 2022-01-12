af5c0e182df7bb4cd1ae18f337e7aae9092e0cef
don kim <dkim05@gmail.com>
2022-01-11 23:43:55 -0500

    used json to store answers

--- js/stage.js
+++ js/stage.js
@@ -1,15 +1,20 @@
+
 $(document).ready(function () {
   $('#codeBtn').click(function(e){
-
     e.preventDefault();
-    var asw = $('#answer1').val();
-
-    if (asw == 'LILY') {
-      console.log('correct');
-      window.location.href = "./stage2-1.html";
-    } else {
-      $('#hintOverlay').css({display: "block"});
-    }
+    // page_index variable - stage_1 value is 0 and stage_2 value is 1 etc.
+    var page_index=$("#answer").attr("value");
+    $.getJSON("./answers.json", function(data) {
+      // test
+      // console.log(data.answers[page_index].answer);
+      // console.log($('#answer').val());
+      if ($('#answer').val() == data.answers[page_index].answer) {
+        console.log('correct');
+        window.location.href = "./"+data.answers[page_index].page;
+      } else {
+        $('#hintOverlay').css({display: "block"});
+      }
+    })
   });
 
   $('#hintCloseBtn').click(function(){
