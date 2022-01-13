da547575807ec0ed129cca0e401c9a9c63eb1fcb
don kim <dkim05@gmail.com>
2022-01-12 22:57:51 -0500

    stage 3 and info modal is moved to common folder

---
+++ js/info.js
@@ -0,0 +1,12 @@
+
+$(function() {
+  $('#infoModal').load('./common/info.html');
+  $(document).on('click', '#infoButton', function(e) {
+    e.preventDefault();
+    $('#infoOverlay').css({display: "block"});
+  });
+  $(document).on('click', '#closeBtn', function(e) {
+    e.preventDefault();
+    $('#infoOverlay').css({display: "none"});
+  });
+});
--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
 $(document).ready(function () {
-  // when code is entered
-  // console.log('ready22');
-  // $('#code-form').submit(function(e){
-
-  // $('#info-button').click(function(){
-  //   console.log('info-clicked');
-  //   $('#info-popup').load('./common/info.html');
-  // });
 
   var txtErr = "<p style='color:red; font-size: 0.8em; margin-bottom: -2em;'>코드를 잘못 입력 되었습니다. <br> 다시 확인하고 입력해 주세요.</p>";
-  $('#infoButton').click(function(){
-    $('#infoOverlay').css({display: "block"});
-  });
-
-  $('#closeBtn').click(function(){
-    $('#infoOverlay').css({display: "none"});
-  });
+  // $('#infoButton').click(function(e){
+  //   e.preventDefault();
+  //   $('#infoOverlay').load('./common/info.html');
+  //   $('#infoOverlay').css({display: "block"});
+  //   // $('#infoModal').css({display: "block"});
+  // });
+  //
+  // $('#closeBtn').click(function(e){
+  //   e.preventDefault();
+  //   $('#infoOverlay').css({display: "none"});
+  // });
 
   $('#codeBtn').click(function(e){
     e.preventDefault();
@@ -33,21 +29,9 @@ $(document).ready(function () {
     }
     console.log('done');
   });
-
+  
   $('#startBtn').click(function(e){
     e.preventDefault();
     window.location.href = "./stage1-1.html";
   });
-
-
 });
-
-
-
-// $('#info-button').click(function(){
-//   $('#info-overlay').css({display: "block"});
-// });
-//
-// $('#closeBtn').click(function(){
-//   $('#info-overlay').css({display: "none"});
-// });
