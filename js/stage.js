6b3d225712bd3238f0bcfd49c20e127ac63ee073
don kim <dkim05@gmail.com>
2022-01-12 23:32:57 -0500

    fixed shadow and stage3 minor issues

--- js/stage.js
+++ js/stage.js
@@ -4,10 +4,14 @@ $(document).ready(function () {
     e.preventDefault();
     // page_index variable - stage_1 value is 0 and stage_2 value is 1 etc.
     var page_index=$("#answer").attr("value");
+    console.log(page_index);
     $.getJSON("./answers.json", function(data) {
+      console.log('clicked');
       // test
       // console.log(data.answers[page_index].answer);
       // console.log($('#answer').val());
+      // var ans = ($('#answer').val() === undefined) ? $('#answer').val() : null;
+      // console.log(ans);
       if ($('#answer').val() == data.answers[page_index].answer) {
         console.log('correct');
         window.location.href = "./"+data.answers[page_index].page;
