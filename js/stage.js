0a6635564b9d902816490b31d0d39bc8308ba198
don kim <dkim05@gmail.com>
2022-01-15 15:23:35 -0500

    stage 4 is implemented . not completed

--- js/stage.js
+++ js/stage.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     var page_index=$("#answer").attr("index");
     console.log(page_index);
     $.getJSON("./answers.json", function(data) {
-      console.log('clicked');
+      // console.log('clicked');
       // test
       // console.log(data.answers[page_index].answer);
       // console.log($('#answer').val());
@@ -14,14 +14,29 @@ $(document).ready(function () {
       // console.log(ans);
       if ($('#answer').val() == data.answers[page_index].answer) {
         console.log('correct');
-        window.location.href = "./"+data.answers[page_index].page;
+        if (page_index == 3) {
+
+          $('#ticketOverlay').css({display: "block"});
+        } else {
+          window.location.href = "./"+data.answers[page_index].page;
+        }
       } else {
         $('#hintOverlay').css({display: "block"});
       }
-    })
+    });
   });
 
   $('#hintCloseBtn').click(function(){
     $('#hintOverlay').css({display: "none"});
   });
+
+  $('#ticketNextBtn').click(function(){
+    $.getJSON("./answers.json", function(data) {
+      var page_index=$("#answer").attr("index");
+      console.log(page_index);
+      $('#ticketOverlay').css({display: "none"});
+      window.location.href = "./"+data.answers[page_index].page;
+    });
+  });
+
 });
