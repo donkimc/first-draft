9faa9ebbb7f61eb367fa290694aa05933db086ad
don kim <dkim05@gmail.com>
2022-02-27 22:13:51 -0500

    background

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+
+
   var txtErr = "<p style='color:red; font-size: 0.8em; margin-bottom: -2em;'>코드를 잘못 입력 되었습니다. <br> 다시 확인하고 입력해 주세요.</p>";
   // $('#infoButton').click(function(e){
   //   e.preventDefault();
