be07d709cba6987fbae74c306d736692c9df0b07
don kim <dkim05@gmail.com>
2022-03-05 22:05:15 -0500

    enter code by pressing enter key

--- js/main.js
+++ js/main.js
@@ -3,33 +3,17 @@ $(document).ready(function () {
 
 
   var txtErr = "<p style='color:red; font-size: 0.8em; margin-bottom: -2em;'>코드를 잘못 입력 되었습니다. <br> 다시 확인하고 입력해 주세요.</p>";
-  // $('#infoButton').click(function(e){
-  //   e.preventDefault();
-  //   $('#infoOverlay').load('./common/info.html');
-  //   $('#infoOverlay').css({display: "block"});
-  //   // $('#infoModal').css({display: "block"});
-  // });
-  //
-  // $('#closeBtn').click(function(e){
-  //   e.preventDefault();
-  //   $('#infoOverlay').css({display: "none"});
-  // });
 
   $('#codeBtn').click(function(e){
     e.preventDefault();
-    var code = $('#codeInput').val();
+    checkCode();
+    // console.log('done');
+  });
 
-    if (code == '181818') {
-      console.log('correct');
-      window.location.href = "./start.html";
+  $('#codeInput').keyup(function(event) {
+    if (event.keyCode === 13) {
+      checkCode();
     }
-    else {
-      // alert('Wrong password, Correct password is 181818.');
-      $('#codeError').html(txtErr);
-      setTimeout(function(){$('#codeError').html("")},3000);
-      $("#codeInput").val("")
-    }
-    console.log('done');
   });
 
   $('#startBtn').click(function(e){
@@ -37,3 +21,18 @@ $(document).ready(function () {
     window.location.href = "./stage1-1.html";
   });
 });
+
+function checkCode() {
+  var code = $('#codeInput').val();
+
+  if (code == '181818') {
+    console.log('correct');
+    window.location.href = "./start.html";
+  }
+  else {
+    // alert('Wrong password, Correct password is 181818.');
+    $('#codeError').html(txtErr);
+    setTimeout(function(){$('#codeError').html("")},3000);
+    $("#codeInput").val("")
+  }
+}
