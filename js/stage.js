1e47d73b7af0316f27b7a56e916272e105d90531
don kim <dkim05@gmail.com>
2022-03-05 22:17:22 -0500

    pressing enter to go next page in answer textbox

--- js/stage.js
+++ js/stage.js
@@ -3,47 +3,18 @@ $(document).ready(function () {
 
   $('#codeBtn').click(function(e){
     e.preventDefault();
-    // page_index variable - stage_1 value is 0 and stage_2 value is 1 etc.
-    var page_index=$("#answer").attr("index");
-    $.getJSON("./answers.json", function(data) {
-      // console.log('clicked');
-      // test
-      // console.log(data.answers[page_index].answer);
-      // console.log($('#answer').val());
-      // var ans = ($('#answer').val() === undefined) ? $('#answer').val() : null;
-      // console.log(ans);
-      if ($('#answer').val() == data.answers[page_index].answer) {
-        console.log('correct');
-        // stage 1
-        if (page_index == 0) {
-          $('#loadingOverlay').css({display: "block"});
-          setTimeout(function(){
-            window.location.href = "./"+data.answers[page_index].page;
-          },4000);
-        }
-        //stage 4
-        else if (page_index == 3) {
-          $('#ticketOverlay').css({display: "block"});
-        }
-        // stage 6
-        else if (page_index == 5) {
-          $('#dangerOverlay').css({display: "block"});
-        } else {
-          window.location.href = "./"+data.answers[page_index].page;
-        }
-      } else {
-        $('#hintOverlay').css({display: "block"});
-      }
-    });
+    checkAnswer();
+  });
+
+  $('#answer').keyup(function(event) {
+    if (event.keyCode === 13) {
+      checkAnswer();
+    }
   });
 
   $('#hintCloseBtn').click(function(){
     $('#hintOverlay').css({display: "none"});
   });
-
-  // stage 1
-
-
 
   // stage 4
   $('#ticketNextBtn').click(function(){
@@ -71,3 +42,33 @@ $(document).ready(function () {
 
 
 });
+
+function checkAnswer() {
+  // page_index variable - stage_1 value is 0 and stage_2 value is 1 etc.
+  var page_index=$("#answer").attr("index");
+  $.getJSON("./answers.json", function(data) {
+
+    if ($('#answer').val() == data.answers[page_index].answer) {
+      console.log('correct');
+      // stage 1
+      if (page_index == 0) {
+        $('#loadingOverlay').css({display: "block"});
+        setTimeout(function(){
+          window.location.href = "./"+data.answers[page_index].page;
+        },4000);
+      }
+      //stage 4
+      else if (page_index == 3) {
+        $('#ticketOverlay').css({display: "block"});
+      }
+      // stage 6
+      else if (page_index == 5) {
+        $('#dangerOverlay').css({display: "block"});
+      } else {
+        window.location.href = "./"+data.answers[page_index].page;
+      }
+    } else {
+      $('#hintOverlay').css({display: "block"});
+    }
+  });
+}
